1be3b2eb3fef6237ae34b49399e45989d10f5f5c
beyzagurler <78827915+beyzagurler@users.noreply.github.com>
2021-05-06 15:56:27 -0400

    trying something out

--- addingcounties.py
+++ addingcounties.py
@@ -12,10 +12,22 @@ import pandas as pd
 
 #opening California Fire Perimeters as a dataframe
 
-fire= pd.read_csv("California_Fire_Perimeters.csv")
+fires= pd.read_csv("California_Fire_Perimeters.csv")
 
 #renaming the missing values under containment date from 1970/01/01 to MISSING
 
-fire["CONT_DATE"]= fire["CONT_DATE"].replace(["1970/01/01 00:00:00+00"],"MISSING")
+fires["CONT_DATE"]= fires["CONT_DATE"].replace(["1970/01/01 00:00:00+00"],"MISSING")
+
+#getting rid of the timestamps
+
+fires["CONT_DATE"]= fires["CONT_DATE"].str.rstrip(" 00:00:00+00")  
+
+fires["ALARM_DATE"]= fires["ALARM_DATE"].str.rstrip(" 00:00:00+00")
+
+
 
 #adding counties 
+
+
+#saving the dataframe "fires" as a csv file for other scripts to use
+fires.to_csv("fires.csv")
--- bins.py
+++ bins.py
@@ -5,4 +5,22 @@ Created on Thu May  6 13:45:07 2021
 
 @author: beyzagurler
 """
+#This script will create bins for the years, decades, and counties for fires in CA
+
+import pandas as pd
+
+#open fires dataframe
+fires= pd.read_csv("fires.csv")
+
+
+#breaking up the dates
+
+pd.to_datetime(fires["ALARM_DATE"])
+print(fires["ALARM_DATE"])
+
+#fires["year"]= 
+
+#fires["month"]=
+
+#fires["day"]=
 
