03b6fab737ddcb05670c405c04031745643def0b
beyzagurler <78827915+beyzagurler@users.noreply.github.com>
2021-05-08 14:19:40 -0400

    stuff is acting up!!! computer might crash and burn

--- bins.py
+++ bins.py
@@ -10,15 +10,37 @@ Created on Thu May  6 13:45:07 2021
 import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas
+import datetime as dt
+import seaborn as sns
 
 #opening fires_by_county 
-fires_by_county= pd.read_csv("fires_by_county.csv")
+fires_by_county= pd.read_csv("fires_by_counties.csv")
 
-#Creating a bin for each decade
+#grouping by decades
 
-1900-2019
+fires_by_county["decade_bins"]= fires_by_county["YEAR_"].round(-1)
 
-1900-1910
-so we wanna do [1900:1910) and then [1910,1920)
+sorted_decades=fires_by_county["decade_bins"].value_counts().sort_index()
+
+print("\nFire Count for each decade:","\n", sorted_decades)
+
+#fires_by_decade=fires_by_county["decade_bins"].groupby()
+
+decades= sns.jointplot(sorted_decades, y="Number of Fires", x='Years', kind='hex')
+
+#decades.savefig('fires_by_decade.png')
+
+
+#print(fires_by_county["decade_bins"])
+
+
+#print("\nFires per Year in California State:", len("YEAR_"))
+
+
+
+
+
+
+
                                      
-                                     
+#make a histogram at the end of this                                     
