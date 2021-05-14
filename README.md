75a06c33ec5e0a59286c90d84e244316024a4602
beyzagurler <78827915+beyzagurler@users.noreply.github.com>
2021-05-14 12:21:29 -0400

    submission2

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Created on Tue May  4 12:41:42 2021
 
 @author: beyzagurler
 """
+This repository analyzes historical fires in California. It demonstrates how fires have been getting more and more prevalent and burn more acres than ever before. It also shows that fires have been impacting places with higher populations. 
 
 Getting the Data:
+
 Perimeters for California fires can be obtained from California's State Geoportal. This data contains both the csv file and the shapefile. The link I utilized is: https://gis.data.ca.gov/datasets/CALFIRE-Forestry::california-fire-perimeters-1/data?geometry=-151.022%2C31.426%2C-87.741%2C43.578&layer=0
 California County Boundaries can also be found on California's State Geoportal. This data also contains both the csv file and the shapefile. The link I used is: https://gis.data.ca.gov/datasets/8713ced9b78a4abb97dc130a691a8695_0?geometry=-150.643%2C31.049%2C-87.361%2C43.258
 Population Data for each county can be obtained from the Census using the population.py script and would only need an individualized key_value.
@@ -23,4 +25,6 @@ The population.py script obtains the population data for each county in Californ
 
 The bins.py scrip starts the analysis. It groups the fires by decades and gives an output of all the fires in California for each decade. It also creates a barplot of the amount of fires by decade.
 
-The groupingbycounties.py script continues the analysis. It gives a list of the top 15 fires in California by the amount of acres burned as well as the top 10 counties with the amount amount of fires. These show that the counties with higher populations tend to have higher amounts of fires (with the exception of Siskiyou County ("093") and Tulare County ("107")). The top 15 fires in California are almost all completely in the more recent years (2002-2018) with the exception of one fire in 1987 in Siskiyou County. The top 15 fires were determined by the amount of acres that were burned in a single incident. 
+The groupingbycounties.py script continues the analysis. It gives a list of the top 15 fires in California by the amount of acres burned as well as the top 10 counties with the amount amount of fires. These show that the counties with higher populations tend to have higher amounts of fires (with the exception of Siskiyou County ("093") and Tulare County ("107")). The top 15 fires in California are almost all completely in the more recent years (2002-2018) with the exception of one fire in 1987 in Siskiyou County. The top 15 fires were determined by the amount of acres that were burned in a single incident. This script creates a few plots. The first one is called Worst_fires.png and is a visualization of the top 15 fires. The graph "acres_over_years.png" shows the amount of acres burned for each year and it can be seen that it has been growing exponentially for more recent years.
+
+Join.qgz also generates a heat map of counties with the most amount of fires. The darker the red on the map is, the higher the count of historical fires. It shows that smaller counties tend to have more fires and also shows how the counties around them are affected. It tells us about which counties are able to contain fires as well as which counties have a harder time with containment (it can be seen in the way the counties around them have higher numbers of fires as well). 
--- groupingbycounties.py
+++ groupingbycounties.py
@@ -27,6 +27,7 @@ number_by_county=number_by_county.to_frame()
 
 number_by_county=number_by_county.reset_index()
 
+#merging the population by county to the number of fires per county
 number_by_county=number_by_county.merge(population, on="COUNTY", how="outer", indicator=True)
 
 #fires in each county each year
@@ -40,91 +41,45 @@ acres_burned_yearly=huge_fire_data.groupby("YEAR_")["GIS_ACRES"].sum()
 #ammount of acres per county in all of time 
 county_tot_acres= total_acres.groupby("COUNTY").sum()
 
+#cleaning up some columns
 number_by_county= number_by_county.drop(["Unnamed: 0", "_merge", "state"], axis=1)
- 
-#heat map of fires to see if some counties have more fires than others, HISTOGRAM OR MAP
-#heat map heat map! counties with a lot of fires might be the big counties 
-#are there a lot more fires in some than others, where are located, 
 
-#COUNT FIRES IN LAST TWENTY YEARS--> scatterplot
-
-#high pop plus high fires
-
-#MEASURE OF FIRES BY LAND AREAS maP OF CALI TAKE ALAND VARIABLE SQ METERS  TAKE NUMBER OF FIRES/SQMETERS gives you number of fires per square meters multiple by 1MILLION Number of fires by 1m sq meters
-#POP DENSITY AND FIRE DENSITY = SCATTER THAT 
 
 #%%
-heatmap_data=number_by_county[['COUNTY', "Fires"]]
-
-fig,ax1= plt.subplots(dpi=300)
-fig.suptitle("TITLE")
-sns.heatmap(heatmap_data, annot=True, fmt=".0f", ax=ax1)
-ax1.set_xlabel("x")
-ax1.set_ylabel("y")
-fig.tight_layout()
-
-#%%
-
+#Seeing when the top 15 fires occured 
 top15fires=total_acres.sort_values("GIS_ACRES").iloc[-15:]
 print("\nTop 15 fires in California:","\n",top15fires)
+
+#seeing the counties that have the most amount of fires
 top10counties=number_by_county.sort_values("Fires").iloc[-10:]
 print("\nTop 10 counties with the most amount of fires:","\n" ,top10counties)
 
 #%%
-#GRAPH THE WORST FIRE BY ACRE AND SEE WHEN IT WAS COMPARED TO THE OTHER ONES
-
+#plotting the worst fires ever
 fig, ax1= plt.subplots()
 sns.barplot(x="YEAR_", y="GIS_ACRES", data=top15fires.reset_index(), ax=ax1)
 plt.ylabel('Acres Burned')
 plt.xlabel('Years')
 plt.title('Worst Fires')
-fig.savefig('WORST.png', dpi=500)
+fig.savefig('Worst_fires.png', dpi=300)
 
 
 #%%
-
+#plotting acres burned over all the years
 fig, ax1= plt.subplots()
 sns.barplot(x="YEAR_", y="GIS_ACRES", data=acres_burned_yearly.reset_index(), ax=ax1)
 plt.ylabel('Acres Burned')
 plt.xlabel('Years')
-plt.xticks(rotation=25)
+plt.xticks(rotation=90, fontsize=3.5)
 plt.title('Acres Burned per Year')
 fig.savefig('Acres_over_years.png', dpi=300)
 
-#%%
-fig, ax1= plt.subplots(dpi=300)
-fig.suptitle("HEATMAAAAAP")
-sns.heatmap(grid, annot=True, fmt=".0f", ax=ax1)
-ax1.set_xlabel("Years")
-ax1.set_ylabel("Acres")
-fig.tight_layout()
-fig.savefig("heatmap.png")
 
 #%%
-
-
-fires_by_county=pd.read_csv("fires_by_counties.csv")
-
-#WHAT I AM TRYING T0 DO IS SEE THE FIRES IN EACH COUNTY IN THE LAST TWO DECADES AND SEE THE POPULATION TOO
-
-last20years=huge_fire_data.sort_values("YEAR_").round(-1)
-print(last20years)
-
-fig, ax1= plt.subplots()
-sns.barplot(x="YEAR_", y="GIS_ACRES", data=last20years.reset_index(), ax=ax1)
-plt.ylabel('Acres Burned')
-plt.xlabel('Years')
-plt.xticks(rotation=25)
-plt.title('Acres Burned per Year')
-fig.savefig('Acres over years.png', dpi=300)
-
-#number_by_county = huge_fire_data.groupby("COUNTY").size()
-
-#%%
-
+#plotting the amount of historical fires for population in counties 
 fires_pop=sns.scatterplot(data = number_by_county , x = "Fires", y = "Population")
-fires_pop.set_title("Amount of Historical Fires per population in counties")
-fires_pop.savefig("population_counties.png")
+fires_pop.set_title("Amount of Historical Fires by population in counties")
+plt.savefig("population_counties.png")
 
 #%%
-sns.heatmap()
+
